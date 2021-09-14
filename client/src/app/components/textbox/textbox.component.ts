281f664933eca1ab0b629db907fae87048ffec4c
2024616 <www1202161@gmail.com>
2021-09-14 17:45:04 -0400

    some tests

---
+++ client/src/app/components/textbox/textbox.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-textbox',
+    templateUrl: './textbox.component.html',
+    styleUrls: ['./textbox.component.scss'],
+})
+export class TextboxComponent {}
