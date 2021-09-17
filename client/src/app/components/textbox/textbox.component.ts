e5641118a92dff76d8882beddb3c1310fe37d807
Chun Li <chun-yang.li@polymtl.ca>
2021-09-17 17:14:36 -0400

    update

--- client/src/app/components/textbox/textbox.component.ts
+++ client/src/app/components/textbox/textbox.component.ts
@@ -5,4 +5,14 @@ import { Component } from '@angular/core';
     templateUrl: './textbox.component.html',
     styleUrls: ['./textbox.component.scss'],
 })
-export class TextboxComponent {}
+export class TextboxComponent {
+    sendMessage(){
+        let message = (<HTMLInputElement>document.getElementById("message")).value;
+        (<HTMLInputElement>document.getElementById("thread")).textContent = "User: " + message;
+
+        //let textArea = document.getElementById("thread");   
+        //let newMessage = document.createTextNode(message)
+        //document.getElementById("thread").innerText += newMessage;
+    }
+    
+}
