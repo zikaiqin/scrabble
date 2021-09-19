02293067b9c3ad00801893380a3cc1bc507b37c4
Zi Kai Qin <zikaiqin99@gmail.com>
2021-09-19 06:20:35 -0400

    interface d'initialisation de partie implemente

---
+++ client/src/app/components/new-game-menu/new-game-menu.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { GameMode, GameType } from '@app/pages/home-page/home-page.component';
+
+@Component({
+    selector: 'app-new-game-menu',
+    templateUrl: './new-game-menu.component.html',
+    styleUrls: ['../../pages/home-page/home-page.component.scss', './new-game-menu.component.scss'],
+})
+export class NewGameMenuComponent {
+    @Input() gameMode: number;
+    @Input() gameType: number;
+    @Output() readonly goBack = new EventEmitter<string>();
+
+    // eslint-disable-next-line @typescript-eslint/no-useless-constructor,@typescript-eslint/no-empty-function
+    constructor() {}
+
+    getTitle(): string {
+        return this.gameMode === GameMode.Classical ? 'Mode Classique' : 'Mode LOG2990';
+    }
+
+    getSubTitle(): string {
+        return this.gameType === GameType.Single ? 'Partie Solo' : 'Partie Multijoueur';
+    }
+
+    timeFormat(value: number) {
+        const minute = 60;
+        if (value >= minute) {
+            const min = Math.floor(value / minute);
+            const sec = value - min * minute;
+            return min + `:${sec === 0 ? '00' : sec}`;
+        }
+        return value + 's';
+    }
+}
