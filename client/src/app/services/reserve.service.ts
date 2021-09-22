02487cc69bd4621517b36f997e6350b6944ffb33
unknown <ying-chen.gao@polymtl.ca>
2021-09-22 15:36:10 -0400

    ajout reserve de lettres

--- client/src/app/services/reserve.service.ts
+++ client/src/app/services/reserve.service.ts
@@ -6,8 +6,34 @@ type Reserve = {
 };
 
 /*const baseReserve: Reserve = {
-  letters: ["a","b","c","d"],
-  quantity: 4
+  letters: ["a", "a", "a", "a", "a", "a", "a", "a", "a", 
+  "b", "b",
+  "c", "c",
+  "d", "d", "d",
+  "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e",
+  "f", "f",
+  "g", "g",
+  "h", "h",
+  "i", "i", "i", "i", "i", "i", "i", "i",
+  "j",
+  "k",
+  "l", "l", "l", "l", "l",
+  "m", "m", "m",
+  "n", "n", "n", "n", "n", "n",
+  "o", "o", "o", "o", "o", "o",
+  "p", "p",
+  "q",
+  "r", "r", "r", "r", "r", "r",
+  "s", "s", "s", "s", "s", "s",
+  "t", "t", "t", "t", "t", "t",
+  "u", "u", "u", "u", "u", "u",
+  "v", "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "*", "*"],
+  quantity: 102
 };*/
 
 //let currentReserve:Reserve = {letters:[], quantity:0};
