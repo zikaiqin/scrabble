f946f2d5fbfd330dc9718cda6309cd3b360ff067
Zi Kai Qin <zikaiqin99@gmail.com>
2021-11-19 03:46:46 -0500

    removed "joueur virtuel" component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,29 +1,27 @@
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
-import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
 import { AppRoutingModule } from '@app/modules/app-routing.module';
 import { AppMaterialModule } from '@app/modules/material.module';
 import { AppComponent } from '@app/pages/app/app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
+import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
 import { ChevaletComponent } from './components/chevalet/chevalet.component';
 import { EndGameComponent } from './components/end-game/end-game.component';
 import { InputbarComponent } from './components/inputbar/inputbar.component';
-import { JoueurVirtuelleComponent } from './components/joueur-virtuelle/joueur-virtuelle.component';
-import { MainMenuComponent } from './components/main-menu/main-menu.component';
-import { NewGameMenuComponent } from './components/new-game-menu/new-game-menu.component';
 import { PanneauInfoComponent } from './components/panneau-info/panneau-info.component';
 import { TextboxComponent } from './components/textbox/textbox.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { NewGameMenuComponent } from './components/new-game-menu/new-game-menu.component';
 import { GameBrowserComponent } from './components/game-browser/game-browser.component';
 import { GameBrowserDialogComponent } from '@app/components/game-browser-dialog/game-browser-dialog.component';
 import { WaitingRoomComponent } from './components/waiting-room/waiting-room.component';
 import { WaitingRoomDialogComponent } from './components/waiting-room-dialog/waiting-room-dialog.component';
-import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 
 /**
  * Main module that is used in main.ts.
@@ -34,34 +32,24 @@ import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 @NgModule({
     declarations: [
         AppComponent,
+        HomePageComponent,
         GamePageComponent,
-        JoueurVirtuelleComponent,
+        AdminPageComponent,
         PlayAreaComponent,
         SidebarComponent,
         ChevaletComponent,
         PanneauInfoComponent,
         TextboxComponent,
         InputbarComponent,
-        HomePageComponent,
+        EndGameComponent,
         MainMenuComponent,
         NewGameMenuComponent,
-        EndGameComponent,
         GameBrowserComponent,
         GameBrowserDialogComponent,
         WaitingRoomComponent,
         WaitingRoomDialogComponent,
-        AdminPageComponent,
     ],
-    imports: [
-        AppMaterialModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        BrowserModule,
-        FormsModule,
-        HttpClientModule,
-        DragDropModule,
-        ReactiveFormsModule,
-    ],
+    imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
 
     providers: [],
     bootstrap: [AppComponent],
