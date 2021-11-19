7f9c7d36156c5c89c510bc96f6d1748ee10b4811
zhao.long <requisssent@gmail.com>
2021-11-19 10:51:25 -0500

    Creation of objectives component

---
+++ client/src/app/components/objectives/objectives.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-objectives',
+    templateUrl: './objectives.component.html',
+    styleUrls: ['./objectives.component.scss'],
+})
+export class ObjectivesComponent {}
