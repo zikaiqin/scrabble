928664d2c65411a7944a75d69987b7066a64178e
Zi Kai Qin <zikaiqin99@gmail.com>
2021-11-22 20:36:17 -0500

    implemented admin page side nav

--- client/src/app/modules/material.module.ts
+++ client/src/app/modules/material.module.ts
@@ -12,14 +12,14 @@ import { MatExpansionModule } from '@angular/material/expansion';
 // import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-// import { MatListModule } from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 // import { MatMenuModule } from '@angular/material/menu';
 // import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 // import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
-// import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -45,14 +45,14 @@ const modules = [
     // MatGridListModule,
     MatIconModule,
     MatInputModule,
-    // MatListModule,
+    MatListModule,
     // MatMenuModule,
     // MatPaginatorModule,
     MatProgressBarModule,
     // MatProgressSpinnerModule,
     MatRadioModule,
     MatSelectModule,
-    // MatSidenavModule,
+    MatSidenavModule,
     MatSliderModule,
     MatSlideToggleModule,
     MatSnackBarModule,
