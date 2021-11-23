91d063270d2280245def0627bb9ea31a0ec0608a
Zhao Long <requisssent@gmail.com>
2021-11-22 22:14:05 -0500

    tests on client side now passes

--- client/src/app/components/objectives/objectives.component.spec.ts
+++ client/src/app/components/objectives/objectives.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ObjectivesComponent } from './objectives.component';
+import { WebsocketService } from '@app/services/websocket.service';
+import { Subject } from 'rxjs';
+import { GameInit } from '@app/classes/game-info';
 
 describe('ObjectivesComponent', () => {
     let component: ObjectivesComponent;
     let fixture: ComponentFixture<ObjectivesComponent>;
 
+    const gameInit = new Subject<GameInit>();
+    const objectives = new Subject<{ publicObj: [number, boolean][]; privateObj: [number, boolean] }>();
+
+    const websocketServiceSpy = jasmine.createSpyObj('WebsocketService', [], {
+        init: gameInit.asObservable(),
+        objective: objectives.asObservable(),
+    });
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [ObjectivesComponent],
+            providers: [{ provide: WebsocketService, useValue: websocketServiceSpy }],
         }).compileComponents();
     });
 
