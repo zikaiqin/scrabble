9445ec520b213cbfdd9a8bbe30d6562faf073145
Zhao Long <requisssent@gmail.com>
2021-11-28 17:45:22 -0500

    refactored a bit the tests to remove some duplicated code

--- server/app/services/socket.service.spec.ts
+++ server/app/services/socket.service.spec.ts
@@ -1,4 +1,5 @@
-/* eslint-disable dot-notation,max-lines */
+/* eslint-disable dot-notation,max-lines,@typescript-eslint/no-explicit-any */
+// we need this disable in order to declare spies (type is too complicated) --> any
 import { SocketService } from '@app/services/socket.service';
 import { Container } from 'typedi';
 import * as http from 'http';
@@ -20,6 +21,8 @@ describe('SocketService', () => {
     let socket: Socket;
     let server: Server;
     let activeRoomGetSpy: Sinon.SinonSpy<[key: string], string | undefined>;
+    let socketEmitSpy: any;
+    let sioEmitSpy: any;
     const sandbox = createSandbox();
 
     beforeEach(() => {
@@ -27,6 +30,8 @@ describe('SocketService', () => {
         fakeHighScoreService = Container.get(HighscoreService);
         service = new SocketService(http.createServer(), fakeHighScoreService);
         activeRoomGetSpy = sandbox.spy(service['activeRooms'], 'get');
+        socketEmitSpy = sandbox.spy(service['socketEvents'], 'emit');
+        sioEmitSpy = sandbox.spy(service['sio'], 'emit');
     });
 
     before(() => {
@@ -44,88 +49,76 @@ describe('SocketService', () => {
             [2, true],
         ];
         const privateObj: [number, boolean] = [3, false];
-        const spy = sandbox.spy(service['sio'], 'emit');
         service.updateObjectives(socketID, publicObj, privateObj);
-        expect(spy.calledWith('updateObjectives', publicObj, privateObj));
+        expect(sioEmitSpy.calledWith('updateObjectives', publicObj, privateObj));
     });
 
     it('deleteRoom should call the right functions', () => {
         const deleteSpy = sandbox.spy(service['waitingRooms'], 'delete');
-        const emitSpy = sandbox.spy(service['sio'], 'emit');
         service.deleteRoom(socketID);
         expect(deleteSpy.calledWith(socketID));
-        expect(emitSpy.calledWith('updateRooms', service.roomList));
+        expect(sioEmitSpy.calledWith('updateRooms', service.roomList));
     });
 
     it('sendMessage should call the right functions', () => {
-        const spy = sandbox.spy(service['sio'], 'emit');
         service.sendMessage(socketID, socketID, socketID);
-        expect(spy.calledWith('receiveMessage', socketID, socketID));
+        expect(sioEmitSpy.calledWith('receiveMessage', socketID, socketID));
     });
 
     it('setConfigs should call the right functions', () => {
         const bonuses: Map<string, string> = new Map<string, string>([[socketID, socketID]]);
         const reserve: string[] = ['a', 'b', 'c', 'd'];
-        const spy = sandbox.spy(service['sio'], 'emit');
         service.setConfigs(socketID, socketID, socketID, bonuses, reserve, reserve, 0, false);
-        expect(spy.calledWith('initGame', socketID, socketID, Array.from(bonuses.entries()), reserve, reserve, 0, false));
+        expect(sioEmitSpy.calledWith('initGame', socketID, socketID, Array.from(bonuses.entries()), reserve, reserve, 0, false));
     });
 
     it('updateTime should call the right functions', () => {
-        const spy = sandbox.spy(service['sio'], 'emit');
         service.updateTime(socketID, 1);
-        expect(spy.calledWith('updateTime', 1));
+        expect(sioEmitSpy.calledWith('updateTime', 1));
     });
 
     it('updateTurn should call the right functions', () => {
-        const spy = sandbox.spy(service['sio'], 'emit');
         service.updateTurn(socketID, true);
-        expect(spy.calledWith('updateTurn', true));
+        expect(sioEmitSpy.calledWith('updateTurn', true));
     });
 
     it('updateBoard should call the right functions', () => {
         const board: Map<string, string> = new Map<string, string>([['a1', 'a']]);
-        const spy = sandbox.spy(service['sio'], 'emit');
         service.updateBoard(socketID, board);
-        expect(spy.calledWith('updateBoard', Array.from(board.entries())));
+        expect(sioEmitSpy.calledWith('updateBoard', Array.from(board.entries())));
     });
 
     it('updateReserve should call the right functions', () => {
         const reserve = ['a', 'b', 'c', 'd'];
-        const spy = sandbox.spy(service['sio'], 'emit');
         service.updateReserve(socketID, reserve);
-        expect(spy.calledWith('updateReserve', reserve));
+        expect(sioEmitSpy.calledWith('updateReserve', reserve));
     });
 
     it('updateHands should call the right functions', () => {
         const ownHand = ['a', 'b', 'c', 'd'];
         const opponentHand = ['e', 'f', 'g', 'h'];
-        const spy = sandbox.spy(service['sio'], 'emit');
         service.updateHands(socketID, ownHand, opponentHand);
-        expect(spy.calledWith('updateHands', ownHand, opponentHand));
+        expect(sioEmitSpy.calledWith('updateHands', ownHand, opponentHand));
     });
 
     it('updateScore should call the right functions', () => {
         const ownScore = 2;
         const opponentScore = 1;
-        const spy = sandbox.spy(service['sio'], 'emit');
         service.updateScores(socketID, ownScore, opponentScore);
-        expect(spy.calledWith('updateScores', ownScore, opponentScore));
+        expect(sioEmitSpy.calledWith('updateScores', ownScore, opponentScore));
     });
 
     it('updateHighScore should call the right functions', () => {
         const collection: Score[] = [new Score('abc', 1)];
-        const spy = sandbox.spy(service['sio'], 'emit');
         service.updateHighscores(collection, 2);
-        expect(spy.calledWith('updateHighscoreLog2990', collection));
+        expect(sioEmitSpy.calledWith('updateHighscoreLog2990', collection));
         service.updateHighscores(collection, 1);
-        expect(spy.calledWith('updateHighscoreClassic', collection));
+        expect(sioEmitSpy.calledWith('updateHighscoreClassic', collection));
     });
 
     it('gameEnded should call the right functions', () => {
-        const spy = sandbox.spy(service['sio'], 'emit');
         service.gameEnded(socketID, socketID);
-        expect(spy.calledWith('gameEnded', socketID));
+        expect(sioEmitSpy.calledWith('gameEnded', socketID));
     });
 
     it('roomList should return correctly', () => {
@@ -161,18 +154,16 @@ describe('SocketService', () => {
         socket = io(environment.serverUrl).connect();
         const configs: GameInfo = { username: 'test', turnLength: 60, randomized: false, gameMode: 1, gameType: 2, difficulty: 1, roomID: socketID };
         const setSpy = sandbox.spy(service['waitingRooms'], 'set');
-        const sioSpy = sandbox.spy(service['sio'], 'emit');
         socket.emit('createGame', configs);
         expect(setSpy.calledWith('_' + socketID, configs));
-        expect(sioSpy.calledWith('updateRooms', service.roomList));
+        expect(sioEmitSpy.calledWith('updateRooms', service.roomList));
     });
 
     it('createGame should call the right functions with SINGLE gameType', () => {
         socket = io(environment.serverUrl).connect();
         const configs: GameInfo = { username: 'test', turnLength: 60, randomized: false, gameMode: 1, gameType: 1, difficulty: 1, roomID: socketID };
-        const socketSpy = sandbox.spy(service['socketEvents'], 'emit');
         socket.emit('createGame', configs);
-        expect(socketSpy.calledWith('_' + socketID, configs, [{ socketID: socket.id, username: configs.username }]));
+        expect(socketEmitSpy.calledWith('_' + socketID, configs, [{ socketID: socket.id, username: configs.username }]));
     });
 
     // 50-50 to make tests in validationService crash because of response
@@ -206,8 +197,6 @@ describe('SocketService', () => {
         socket = io(environment.serverUrl).connect();
         const waitingRoomGetSpy = sandbox.spy(service['waitingRooms'], 'get');
         const waitingRoomDeleteSpy = sandbox.spy(service['waitingRooms'], 'delete');
-        const emitSpy = sandbox.spy(service['socketEvents'], 'emit');
-        const sioEmitSpy = sandbox.spy(service['sio'], 'emit');
 
         service['activeRooms'].set(socket.id, socketID);
         const configs: GameInfo = { username: 'test', turnLength: 60, randomized: false, gameMode: 1, gameType: 1, difficulty: 1, roomID: socketID };
@@ -219,7 +208,7 @@ describe('SocketService', () => {
         expect(waitingRoomGetSpy.returned(configs));
         expect(waitingRoomDeleteSpy.calledWith(socketID));
         expect(waitingRoomDeleteSpy.returned(true));
-        expect(emitSpy.calledWith('createGame', socketID, configs, [{ socketID: socket.id, username: configs.username }]));
+        expect(socketEmitSpy.calledWith('createGame', socketID, configs, [{ socketID: socket.id, username: configs.username }]));
         expect(sioEmitSpy.calledWith('updateRooms', service.roomList));
     });
 
@@ -227,8 +216,6 @@ describe('SocketService', () => {
         socket = io(environment.serverUrl).connect();
         const waitingRoomGetSpy = sandbox.spy(service['waitingRooms'], 'get');
         const waitingRoomDeleteSpy = sandbox.spy(service['waitingRooms'], 'delete');
-        const emitSpy = sandbox.spy(service['socketEvents'], 'emit');
-        const sioEmitSpy = sandbox.spy(service['sio'], 'emit');
 
         service['waitingRooms'].clear();
         socket.emit('convertGame', 1);
@@ -236,7 +223,7 @@ describe('SocketService', () => {
         expect(activeRoomGetSpy.returned(undefined));
         expect(waitingRoomGetSpy.notCalled);
         expect(waitingRoomDeleteSpy.notCalled);
-        expect(emitSpy.notCalled);
+        expect(socketEmitSpy.notCalled);
         expect(sioEmitSpy.notCalled);
     });
 
@@ -244,8 +231,6 @@ describe('SocketService', () => {
         socket = io(environment.serverUrl).connect();
         const waitingRoomGetSpy = sandbox.spy(service['waitingRooms'], 'get');
         const waitingRoomDeleteSpy = sandbox.spy(service['waitingRooms'], 'delete');
-        const emitSpy = sandbox.spy(service['socketEvents'], 'emit');
-        const sioEmitSpy = sandbox.spy(service['sio'], 'emit');
 
         service['activeRooms'].set(socket.id, socketID);
         service['waitingRooms'].clear();
@@ -255,13 +240,12 @@ describe('SocketService', () => {
         expect(waitingRoomGetSpy.calledWith(socketID));
         expect(waitingRoomGetSpy.returned(undefined));
         expect(waitingRoomDeleteSpy.notCalled);
-        expect(emitSpy.notCalled);
+        expect(socketEmitSpy.notCalled);
         expect(sioEmitSpy.notCalled);
     });
 
     it('sendMessage should call the right functions', () => {
         socket = io(environment.serverUrl).connect();
-        const socketEmitSpy = sandbox.spy(socket, 'emit');
 
         service['activeRooms'].set(socket.id, socketID);
         socket.emit('sendMessage', 'Hello');
@@ -272,7 +256,6 @@ describe('SocketService', () => {
 
     it('sendMessage should return', () => {
         socket = io(environment.serverUrl).connect();
-        const socketEmitSpy = sandbox.spy(socket, 'emit');
 
         socket.emit('sendMessage', 'Hello');
         expect(activeRoomGetSpy.calledWith(socket.id));
@@ -282,7 +265,6 @@ describe('SocketService', () => {
 
     it('place should call the right functions', () => {
         socket = io(environment.serverUrl).connect();
-        const socketEmitSpy = sandbox.spy(service['socketEvents'], 'emit');
 
         service['activeRooms'].set(socket.id, socketID);
         socket.emit('place', 'a1', ['a1', 'b']);
@@ -294,7 +276,6 @@ describe('SocketService', () => {
 
     it('place should return', () => {
         socket = io(environment.serverUrl).connect();
-        const socketEmitSpy = sandbox.spy(service['socketEvents'], 'emit');
 
         socket.emit('place', 'a1', ['a1', 'b']);
         expect(activeRoomGetSpy.calledWith(socket.id));
@@ -304,7 +285,6 @@ describe('SocketService', () => {
 
     it('exchange should call the right functions', () => {
         socket = io(environment.serverUrl).connect();
-        const socketEmitSpy = sandbox.spy(service['socketEvents'], 'emit');
 
         service['activeRooms'].set(socket.id, socketID);
         socket.emit('exchange', 'a1');
@@ -315,7 +295,6 @@ describe('SocketService', () => {
 
     it('exchange should return', () => {
         socket = io(environment.serverUrl).connect();
-        const socketEmitSpy = sandbox.spy(service['socketEvents'], 'emit');
 
         socket.emit('exchange', 'a1');
         expect(activeRoomGetSpy.calledWith(socket.id));
@@ -325,7 +304,6 @@ describe('SocketService', () => {
 
     it('skipTurn should call the right functions', () => {
         socket = io(environment.serverUrl).connect();
-        const socketEmitSpy = sandbox.spy(service['socketEvents'], 'emit');
 
         service['activeRooms'].set(socket.id, socketID);
         socket.emit('skipTurn');
@@ -336,11 +314,40 @@ describe('SocketService', () => {
 
     it('skipTurn should return', () => {
         socket = io(environment.serverUrl).connect();
-        const socketEmitSpy = sandbox.spy(service['socketEvents'], 'emit');
 
         socket.emit('skipTurn');
         expect(activeRoomGetSpy.calledWith(socket.id));
         expect(activeRoomGetSpy.returned(undefined));
         expect(socketEmitSpy.notCalled);
+    });
+
+    it('fetchObjectives should call the right functions', () => {
+        socket = io(environment.serverUrl).connect();
+
+        service['activeRooms'].set(socket.id, socketID);
+        socket.emit('fetchObjectives');
+        expect(activeRoomGetSpy.calledWith(socket.id));
+        expect(activeRoomGetSpy.returned(socketID));
+        expect(socketEmitSpy.calledWith('updateObjectives', socket.id, socketID));
+    });
+
+    it('fetchObjectives should return', () => {
+        socket = io(environment.serverUrl).connect();
+
+        socket.emit('fetchObjectives');
+        expect(activeRoomGetSpy.calledWith(socket.id));
+        expect(activeRoomGetSpy.returned(undefined));
+        expect(socketEmitSpy.notCalled);
+    });
+
+    it('refresh should call the right functions', () => {
+        socket = io(environment.serverUrl).connect();
+        const highScoreSpy = sandbox.spy(service['highscoreService'], 'getHighscore');
+
+        socket.emit('refresh');
+        expect(highScoreSpy.calledWith(2));
+        expect(sioEmitSpy.calledWith('updateHighscoreLog2990'));
+        expect(highScoreSpy.calledWith(1));
+        expect(sioEmitSpy.calledWith('updateHighscoreClassic'));
     });
 });
